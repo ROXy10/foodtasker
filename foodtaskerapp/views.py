ba4720bf26999762883fb6377e637b3287100df8
ROXy <djeffik@gmail.com>
2017-03-01 01:49:34 +0200

    views: add restaurant home

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
 def home(request):
+    return redirect(restaurant_home)
+
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
     context = {
 
     }
-    return render(request, 'home.html', context)
+    return render(request, 'restaurant/home.html', context)
