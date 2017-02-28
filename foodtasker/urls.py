48fa15aa7ff904923a90909c2c5385a1d865429c
ROXy <djeffik@gmail.com>
2017-03-01 01:48:58 +0200

    urls: fix

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^restaurant/sign-in/$', auth_views.login, {'template_name': 'restaurant/sign_in.html'},
         name='restaurant-sign-in'),
-    url(r'^restaurant/sign-out/$', auth_views.logout, {'next_page': '/'},
+    url(r'^restaurant/sign-out', auth_views.logout, {'next_page': '/'},
         name='restaurant-sign-out'),
-    url(r'^restaurant$', views.restaurant_home, name='restaurant_home')
+    url(r'^restaurant/$', views.restaurant_home, name='restaurant-home'),
 ]
