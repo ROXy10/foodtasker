e0cbdc10a3c03b0fd83b604794d3b756f2d64bfe
ROXy <djeffik@gmail.com>
2017-03-01 21:49:26 +0200

    views: add validation form in restaurant sing up

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from .forms import UserForm, RestaurantForm
@@ -18,7 +20,24 @@ def restaurant_home(request):
 
 def restaurant_sing_up(request):
     user_form = UserForm()
-    restaurant_form = RestaurantForm
+    restaurant_form = RestaurantForm()
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        restaurant_form = RestaurantForm(request.POST, request.FILES)
+
+        if user_form.is_valid() and restaurant_form.is_valid():
+            new_user = User.objects.create_user(**user_form.cleaned_data)
+            new_restaurant = restaurant_form.save(commit=False)
+            new_restaurant.user = new_user
+            new_restaurant.save()
+
+            login(request, authenticate(
+                username=user_form.cleaned_data['username'],
+                password=user_form.cleaned_data['password']
+            ))
+
+            return redirect(restaurant_home)
 
     context = {
         'user_form': user_form,
