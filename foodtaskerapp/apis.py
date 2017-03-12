bcb7c6f2f5ba2c2f1c4007753de8b7da7e9302bc
ROXy <djeffik@gmail.com>
2017-03-12 23:33:09 +0200

    serializers: add meal

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -1,7 +1,13 @@
-from django.http import JsonResponse
+import json
 
-from .models import Restaurant
-from .serializers import RestaurantSerializer
+from django.utils import timezone
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from oauth2_provider.models import AccessToken
+
+from .models import Restaurant, Meal, Order, OrderDetail
+from .serializers import RestaurantSerializer, MealSerializer
 
 
 def customer_get_restaurants(request):
@@ -19,13 +25,77 @@ def customer_get_restaurants(request):
     return JsonResponse(context)
 
 
-def customer_get_meals(request):
+def customer_get_meals(request, restaurant_id):
+    meals = MealSerializer(
+        Meal.objects.filter(restaurant_id=restaurant_id).order_by('-id'),
+        many=True,
+        context={
+            'request': request,
+        }
+    ).data
+
     context = {
+        'meals': meals,
     }
     return JsonResponse(context)
 
 
+@csrf_exempt
 def customer_add_order(request):
+    """
+    :param:
+        access_token
+        restaurant_id
+        address
+        order_details (json format), example:
+            [{"meal_id": 2, "quantity": 2}, {"meal_id": 3, "quantity": 3}]
+        stripe_token
+    :return:
+        {'status': 'success'}
+    """
+    if request.method == 'POST':
+        # Get token
+        access_token = AccessToken.objects.get(token=request.POST.get('access_token'), expires__gt=timezone.now())
+
+        # Get profile
+        customer = access_token.user.customer
+
+        # Check whether customer has any order that is not delivered
+        if Order.objects.filter(customer=customer).exclude(status=Order.DELIVERED):
+            return JsonResponse({'status': 'failed', 'error': 'Your last order must be completed.'})
+
+        # Check address
+        if not request.POST['address']:
+            return JsonResponse({'status': 'failed', 'error': 'Address is required'})
+
+        # Get Order Detail
+        order_details = json.loads(request.POST['order_details'])
+
+        order_total = 0
+        for meal in order_details:
+            order_total += Meal.objects.get(id=meal['meal_id']).price * meal['quantity']
+
+        if len(order_details) > 0:
+            # Step 1 - Create an Order
+            order = Order.objects.create(
+                customer=customer,
+                restaurant_id=request.POST['restaurant_id'],
+                total=order_total,
+                status=Order.COOKING,
+                address=request.POST['address']
+            )
+
+            # Step 2 - Create order details
+            for meal in order_details:
+                OrderDetail.objects.create(
+                    order=order,
+                    meal_id=meal['meal_id'],
+                    quantity=meal['quantity'],
+                    sub_total=Meal.objects.get(id=meal['meal_id']).price * meal['quantity']
+                )
+
+            return JsonResponse({'status': 'success'})
+
     context = {
     }
     return JsonResponse(context)
--- foodtaskerapp/serializers.py
+++ foodtaskerapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Restaurant
+from .models import Restaurant, Meal
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -14,3 +14,16 @@ class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = ('id', 'name', 'phone', 'address', 'logo')
+
+
+class MealSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, meal):
+        request = self.context.get('request')
+        logo_urls = meal.image.url
+        return request.build_absolute_uri(logo_urls)
+
+    class Meta:
+        model = Meal
+        fields = ('id', 'name', 'short_description', 'image', 'price')
