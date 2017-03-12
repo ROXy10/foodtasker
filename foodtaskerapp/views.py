4c13bf22a345cbbaa8e429017079dce3dfc1e73f
ROXy <djeffik@gmail.com>
2017-03-12 17:56:32 +0200

    views: add order action

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -81,6 +81,12 @@ def restaurant_edit_meal(request, meal_id):
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_order(request):
     orders = Order.objects.filter(restaurant=request.user.restaurant).order_by('-id')
+    if request.method == 'POST':
+        order = Order.objects.get(id=request.POST['id'], restaurant=request.user.restaurant)
+
+        if order.status == Order.COOKING:
+            order.status = Order.READY
+            order.save()
 
     context = {
         'orders': orders,
