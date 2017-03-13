b6a06f3473459c9169755935aee3c1b21c8c7c6c
ROXy <djeffik@gmail.com>
2017-03-13 23:39:39 +0200

    serializers: add order

--- foodtaskerapp/serializers.py
+++ foodtaskerapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Restaurant, Meal
+from .models import Restaurant, Meal, Customer, Driver, Order, OrderDetail
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -27,3 +27,52 @@ class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
         fields = ('id', 'name', 'short_description', 'image', 'price')
+
+
+# ORDER SERIALIZER
+class OrderCustomerSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source='user.get_full_name')
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'name', 'avatar', 'phone', 'address')
+
+
+class OrderDriverSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source='user.get_full_name')
+
+    class Meta:
+        model = Driver
+        fields = ('id', 'name', 'avatar', 'phone', 'address')
+
+
+class OrderRestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ('id', 'name', 'phone', 'address')
+
+
+class OrderMealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = ('id', 'name', 'price')
+
+
+class OrderOrderDetailsSerializer(serializers.ModelSerializer):
+    meal = OrderMealSerializer
+
+    class Meta:
+        model = OrderDetail
+        fields = ('id', 'meal', 'quantity', 'sub_total')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    customer = OrderCustomerSerializer()
+    driver = OrderDriverSerializer()
+    restaurant = OrderRestaurantSerializer()
+    order_details = OrderOrderDetailsSerializer(many=True)
+    status = serializers.ReadOnlyField(source='get_status_display')
+
+    class Meta:
+        model = Order
+        fields = ('id', 'customer', 'restaurant', 'driver', 'order_details', 'total', 'status', 'address')
